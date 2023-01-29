ef43c764a231fffd456413e80cb05908955df6aa
Henry Brausen <hbrausen@ualberta.ca>
2023-01-29 11:18:10 -0700

    Basic life program appears to work

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
 import random
-import time
-import os
-
-from rich.live import Live
-from rich.table import Table
-from rich.panel import Panel
-
-import random
 
 class cell:
     def __init__(self, alive=False):
@@ -19,13 +11,13 @@ class cell:
         return self.alive
 
 class board:
-    def __init__(self, N=40):
+    def __init__(self, N=20):
         self.N = N
         self.cells = [[cell() for i in range(N)] for j in range (N)]
     def randomize(self):
         for r in self.cells:
             for c in r:
-                if random.randint(0,1) == 1:
+                if random.randint(0,3) == 1:
                    c.birth()
                 else:
                     c.kill()
@@ -33,9 +25,11 @@ class board:
         self.cells[r][c].birth()
     def kill_cell(self, r, c):
         self.cells[r][c].kill()
+    def is_alive(self, r, c):
+        return self.cells[r][c].is_alive()
     def get_wrapped_coord(self, r, c):
-        if r > self.N: r %= self.N
-        if c > self.N: c %= self.N
+        if r >= self.N: r %= self.N
+        if c >= self.N: c %= self.N
         if r < 0: r = (self.N-r)%self.N
         if c < 0: c = (self.N-c)%self.N
         return (r,c)
@@ -53,17 +47,27 @@ class board:
     def get_N(self):
         return self.N
     def clone(self):
-        new_board=self.__init__()
-        for r in self.cells:
-            for c in r:
-                if c.is_alive():
-                    new_board[r][c].birth()
+        new_board=board()
+        for r in range(self.N):
+            for c in range(self.N):
+                if self.is_alive(r,c):
+                    new_board.birth_cell(r,c)
         return new_board
+    def quick_print(self):
+        for r in range(self.N):
+            for c in range(self.N):
+                if self.is_alive(r,c):
+                    print('#',end='')
+                else:
+                    print(' ',end='')
+            print()
+        print('-'*self.N)
 
 class life:
     def __init__(self):
         self.board = board()
         self.N = self.board.N
+        self.board.randomize()
     def iterate(self):
         newboard = self.board.clone()
         for r in range(self.N):
@@ -73,5 +77,10 @@ class life:
                     newboard.birth_cell(r,c)
                 else:
                     newboard.kill_cell(r,c)
-        self = newboard
+        self.board = newboard
+        newboard.quick_print()
 
+l = life()
+l.iterate()
+l.iterate()
+l.iterate()
